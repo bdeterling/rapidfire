6e4540bfc879d5e3b58f9b66e233a2da28b8b807
Abram Bailey <funkdified@gmail.com>
2014-08-19 19:49:08 -0500

    Only allow one response to a given survey per user

--- app/models/rapidfire/answer_group.rb
+++ app/models/rapidfire/answer_group.rb
@@ -3,7 +3,9 @@ module Rapidfire
     belongs_to :question_group
     belongs_to :user, polymorphic: true
     has_many   :answers, inverse_of: :answer_group, autosave: true
-
+    
+    validates :user_id, presence: true, uniqueness: { scope: :question_group }
+    
     if Rails::VERSION::MAJOR == 3
       attr_accessible :question_group, :user
     end
