b494017362149cb9faccf5f0eb7314d4a8a9ab97
Abram Bailey <funkdified@gmail.com>
2014-08-19 20:18:17 -0500

    Update answer_group.rb

--- app/models/rapidfire/answer_group.rb
+++ app/models/rapidfire/answer_group.rb
@@ -4,7 +4,7 @@ module Rapidfire
     belongs_to :user, polymorphic: true
     has_many   :answers, inverse_of: :answer_group, autosave: true
     
-    validates :user_id, presence: true, uniqueness: { scope: :question_group }, :message => "Sorry there was an error. You have already submitted this survey."
+    validates :user, presence: true, uniqueness: { scope: :question_group }, :message => "Sorry there was an error. You have already submitted this survey."
     
     if Rails::VERSION::MAJOR == 3
       attr_accessible :question_group, :user
